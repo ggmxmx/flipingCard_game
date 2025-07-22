3c22d29f8b5bfa8e165e5b680af51d782cfecba9
ggmxmx <80310226+ggmxmx@users.noreply.github.com>
2025-07-22 15:52:44 +0300

    nothing

--- flipingCard_game/src/components/CardsContainer.jsx
+++ flipingCard_game/src/components/CardsContainer.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import Card from "./card";
 import './Card.css'
 import CardsArray from "../items";
+import { useState } from "react";
 
 function CardsContainer(){
+
+    const [isFlipped , setFlipped] = useState(false)
     return(
-        <div className="cards-fields">
+        <div className="cards-fields" >
 
               {CardsArray.map((item)=> (
-      <Card image = {item.image}/>
+      <Card image = {isFlipped? item.image : item.back}/>
     ))}
 
         </div>
